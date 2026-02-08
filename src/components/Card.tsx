9285aec758eca3bac5d50045029d1097662e4bdb
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-02-08 12:27:08 +0100

    fix warnung

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -25,11 +25,11 @@ export default function Card({
           {title ?? "Noteworthy technology acquisitions 2021"}
         </h5>
 
-        <p className="mb-3 dark:text-gray-400">
+        <div className="mb-3 dark:text-gray-400">
           {description ??
             children ??
             "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."}
-        </p>
+        </div>
       </div>
     </article>
   );
