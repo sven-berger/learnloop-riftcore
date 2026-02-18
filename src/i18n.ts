9af76fc49a43c8fe5de9bbaa1e5970e6bce15f20
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-02-18 13:24:04 +0100

    remove english translation

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,14 +2,10 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import deDE from "./languages/de-DE";
-import enUS from "./languages/en-US";
 
 const resources = {
   de: {
     translation: deDE,
-  },
-  en: {
-    translation: enUS,
   },
 } as const;
 
